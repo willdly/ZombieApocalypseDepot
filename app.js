a998b06bd2f9d016ce9241d752a1db6364695fc7
William Ly <lyw@flip1.engr.oregonstate.edu>
2022-11-17 01:25:12 -0800

    Add category table and create function

--- app.js
+++ app.js
@@ -70,8 +70,28 @@ app.get('/products', function(req, res)
 
 app.get('/categories', function(req, res)
 {
-    res.render('Categories');                    
+    // Declare Query 1
+    let query1;
 
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.last_name === undefined)
+    {
+        query1 = "SELECT * FROM Categories;";
+    }
+
+    // If there is a query string, we assume this is a search, and return desired results
+    else
+    {
+        query1 = `SELECT * FROM Categories WHERE category_name LIKE "${req.query.category_name}%"`
+    }
+
+    // Run the 1st query
+    db.pool.query(query1, function(error, rows, fields){
+        
+        // Save the people
+        let categories = rows;
+        return res.render('Categories', {data: categories});
+    })
 });
 
 app.get('/products_in_orders', function(req, res)
@@ -192,6 +212,47 @@ let selectCustomer= `SELECT * FROM Customers WHERE customer_id = ?`
                 })
             }
 })});
+
+app.post('/add-category-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Categories (category_name, category_description) VALUES ('${data.category_name}', '${data.category_description}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on Customers
+            query2 = `SELECT Categories.category_id, Categories.category_name, Categories.category_description FROM Categories;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
     
 /*
     LISTENER
---
+++ public/js/add_category.js
@@ -0,0 +1,95 @@
+// Get the objects we need to modify
+let addCategoryForm = document.getElementById('add-category-form-ajax');
+
+// Modify the objects we need
+addCategoryForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputCategoryName = document.getElementById("input-category_name");
+    let inputCategoryDescription = document.getElementById("input-category_description");
+
+    // Get the values from the form fields
+    let categoryNameValue = inputCategoryName.value;
+    let categoryDescriptionValue = inputCategoryDescription.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        category_name: categoryNameValue,
+        category_description: categoryDescriptionValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-category-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputCategoryName.value = '';
+            inputCategoryDescription.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Customers
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("categories-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and cells
+    let row = document.createElement("TR");
+    let categoryIdCell = document.createElement("TD");
+    let categoryNameCell = document.createElement("TD");
+    let categoryDescriptionCell = document.createElement("TD");
+
+    let deleteCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    categoryIdCell.innerText = newRow.category_id;
+    categoryNameCell.innerText = newRow.category_name;
+    categoryDescriptionCell.innerText = newRow.category_description;
+
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function(){
+        deleteCustomer(newRow.category_id);
+    };
+
+    // Add the cells to the row 
+    row.appendChild(categoryIdCell);
+    row.appendChild(categoryNameCell);
+    row.appendChild(categoryDescriptionCell);
+    row.appendChild(deleteCell);
+
+    // Add a row attribute so the deleteRow function can find a newly added row
+    row.setAttribute('data-value', newRow.category_id);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
